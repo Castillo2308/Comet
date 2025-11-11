b533ca9427b19cc6c986639bc83bf93f4cb13d82
Castillo2308 <castillo2308@github.com>
2025-11-11 09:02:57 -0600

    remove: eliminar PWAInstallPrompt del proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ import Events from './pages/Events';
 import RedPoints from './pages/RedPoints';
 import Layout from './components/Layout';
 import SplashScreen from './components/SplashScreen';
-import PWAInstallPrompt from './components/PWAInstallPrompt';
 import { startServiceWorkerNewsChecks, stopServiceWorkerNewsChecks } from './lib/swClient';
 
 function AppRoutes() {
@@ -126,7 +125,6 @@ function App() {
         <DriverServiceProvider>
           <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
             <AppRoutes />
-            <PWAInstallPrompt />
           </div>
         </DriverServiceProvider>
       </AuthProvider>
